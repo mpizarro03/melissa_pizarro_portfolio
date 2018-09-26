18091f87f590cba3e3f0207055a0db51462eb758
Melissa Pizarro <mpizarro03@gmail.com>
2018-09-26 14:42:03 -0600

    one page portfolio skeleton set up

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+
+    //RB popup Modal
+    $('.rb-action').on("click", function() {
+      console.log("click worked")
+      $('#rbModal').modal('show')
+      $('#rbModal').modal('handleUpdate')
+
+    })
+
+    //XG popup Modal
+    $('.xg-action').on("click", function() {
+      console.log("xg click worked")
+
+      $('#xgModal').modal('show')
+      $('#xgModal').modal('handleUpdate')
+    })
+
+    //KTG popup Modal
+    $('.ktg-action').on("click", function() {
+      console.log("xg click worked")
+
+      $('#ktgModal').modal('show')
+      $('#ktgModal').modal('handleUpdate')
+    })
+
+})
