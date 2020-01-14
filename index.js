3032473dba72211d25d38e1ee6879d8342c69c63
MELISSA PIZARRO <mpizarro03@gmail.com>
2020-01-14 12:42:06 -0700

    add updated resume, add new portfolio piece with images, creae assets folder and fix imports

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  //RB popup Modal
+  $(".rb-action").on("click", function() {
+    console.log("click worked");
+    $("#rbModal").modal("show");
+    $("#rbModal").modal("handleUpdate");
+  });
 
+  //XG popup Modal
+  $(".xg-action").on("click", function() {
+    console.log("xg click worked");
 
-    //RB popup Modal
-    $('.rb-action').on("click", function() {
-      console.log("click worked")
-      $('#rbModal').modal('show')
-      $('#rbModal').modal('handleUpdate')
+    $("#xgModal").modal("show");
+    $("#xgModal").modal("handleUpdate");
+  });
 
-    })
+  //KTG popup Modal
+  $(".ktg-action").on("click", function() {
+    console.log("xg click worked");
 
-    //XG popup Modal
-    $('.xg-action').on("click", function() {
-      console.log("xg click worked")
+    $("#ktgModal").modal("show");
+    $("#ktgModal").modal("handleUpdate");
+  });
 
-      $('#xgModal').modal('show')
-      $('#xgModal').modal('handleUpdate')
-    })
+  //Zathom popup Modal
+  $(".zathom-action").on("click", function() {
+    console.log("zathom click worked");
 
-    //KTG popup Modal
-    $('.ktg-action').on("click", function() {
-      console.log("xg click worked")
-
-      $('#ktgModal').modal('show')
-      $('#ktgModal').modal('handleUpdate')
-    })
-
-    //future project popup Modal
-    $('.future-action').on("click", function() {
-      console.log("xg click worked")
-
-      // $('#futureModal').modal('show')
-      // $('#futureModal').modal('handleUpdate')
-    })
-
-
-})
+    $("#zathomModal").modal("show");
+    $("#zathomModal").modal("handleUpdate");
+  });
+});
